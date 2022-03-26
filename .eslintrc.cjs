d1174b9d131e9c6fe7def789a348188aa7907c52
based64.eth <based64.eth@protonmail.com>
2022-03-26 15:54:58 +0800

    feat: add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,18 @@
 module.exports = {
-	root: true,
-	extends: [
-    'eslint:recommended', 
-    'plugin:tailwindcss/recommended', 
-    'prettier',
-  ],
-	plugins: [
-    'svelte3', 
-    'tailwind',
-  ],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+  root: true,
+  extends: ["eslint:recommended", "plugin:tailwindcss/recommended", "prettier", "plugin:storybook/recommended"],
+  plugins: ['svelte3', 'tailwind'],
+  overrides: [{
+    files: ['*.svelte'],
+    processor: 'svelte3/svelte3'
+  }],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  }
 };
