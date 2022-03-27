450d1b4184974e3a64ff62b0e35f4bf7f276ffc5
based64.eth <based64.eth@protonmail.com>
2022-03-27 11:34:05 +0800

    refactor: dead code

--- src/lib/services/web3/createSiweMessage.js
+++ src/lib/services/web3/createSiweMessage.js
@@ -1,10 +1,8 @@
 import { SiweMessage } from 'siwe';
-import walletProvider from '$lib/services/web3/walletProvider'
 
 export default async (address, statement, chainId, nonceUri) => {
   const domain = window.location.host;
   const uri = window.location.origin;
-  const signer = walletProvider.getSigner();
 
   const nonce = await fetch(nonceUri, {
     credentials: 'include'
